d4e9ef14908ebfa87c06b00219c18aab08f69229
JhonDev365 <jhonarcila365@gmail.com>
2022-06-21 13:53:55 -0500

    Implementing GET & POST

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=1524e679-c8da-496b-ad90-2bd4cad93b33'; 
 
+
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=3&api_key=1524e679-c8da-496b-ad90-2bd4cad93b33'; 
+
+const spanError = document.getElementById('error');
 // consumir Api rest con fetch
 // fetch(URL)
 // .then(res => res.json())
@@ -8,18 +12,24 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=1524
 //     img.src = data[0].url;
 // });
 
-async function reload() {
+async function loadRandomCats() {
     const res = await fetch(API_URL);
     const data = await res.json();
     // const img = document.querySelector('img');
     // img.src = data[0].url;
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
 
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    if (res.status !== 200){
+        spanError.innerHTML = `Error ${res.status} in Catland. ${data.message}.`
+    } else {
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
+
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+        img3.src = data[2].url;
+    }
+    
 
     // codigo mas corto
     // const images = document.getElementsByTagName("img"); 
@@ -31,4 +41,23 @@ async function reload() {
 
 }
 
-reload();
+async function loadFavoritesCats() {
+    const res = await fetch(API_URL_FAVORITOS); 
+    const data = await res.json();
+    
+    if (res.status !== 200){
+        spanError.innerHTML = `Error ${res.status} in Catland`
+    } 
+
+}
+
+async function saveFavouriteCat() {
+    const rest = await fetch(API_URL_FAVORITOS, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify({image_id: 'dje'}),
+    });
+}    
+
+loadRandomCats();
+loadFavoritesCats();
